c81cab9d851d16e68d081bef3a6e76ed329144df
andrew-dinunzio <34455143+andrew-dinunzio@users.noreply.github.com>
2018-08-20 16:44:42 -0400

    Update delete-crds.sh
    Don't use aliases

--- istio/delete-crds.sh
+++ istio/delete-crds.sh
@@ -2,4 +2,4 @@
 
 # delete Istio CRDs
 echo "Deleting Istio CRDs..."
-kg crd -o jsonpath='{.items[*].metadata.name}' | xargs -n 1 | grep "istio" | xargs kubectl delete crd
+kubectl get crd -o jsonpath='{.items[*].metadata.name}' | xargs -n 1 | grep "istio" | xargs kubectl delete crd
